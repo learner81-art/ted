f881ec8c6ca123cba222525b04a0627f3ef587e0
zhouyue <zhouyue@jybdshop.cn>
2025-05-08 18:56:14 +0800

    Auto commit: 2025-05-08 18:56:14

--- speakers_web.py
+++ speakers_web.py
@@ -520,7 +520,7 @@ def talk_detail(talk_id):
                 t.eng_content,
                 t.speaker_name_zh as title_zh,
                 t.content as content,
-                t.core_viewpoint as content_display,
+                t.chinese_content as content_display,
                 t.page_count
             FROM speakers s
             JOIN talks t ON s.id = t.speaker_id
